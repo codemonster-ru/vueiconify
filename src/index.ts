d4a614a1c946c72d35ab241cdbd011c68fa16359
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2024-07-26 20:15:22 +0700

    Added moon icon.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { default as CmSunIcon } from './sun/sun.vue';
+import { default as CmMoonIcon } from './moon/moon.vue';
 import { default as CmBarsIcon } from './bars/bars.vue';
 import { default as CmChevronUpIcon } from './chevronUp/chevronUp.vue';
 import { default as CmChevronDownIcon } from './chevronDown/chevronDown.vue';
@@ -7,6 +8,7 @@ import { default as CmChevronRightIcon } from './chevronRight/chevronRight.vue';
 
 export {
     CmSunIcon,
+    CmMoonIcon,
     CmBarsIcon,
     CmChevronUpIcon,
     CmChevronDownIcon,
