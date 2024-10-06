916d28118f604e762228b44537fd150c4c5a71e5
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2024-10-07 01:21:04 +0700

    Changed src.

---
+++ src/lib/index.ts
@@ -0,0 +1,23 @@
+export * from '@/lib/components/sun.vue';
+export { default as CmSunIcon } from '@/lib/components/sun.vue';
+
+export * from '@/lib/components/moon.vue';
+export { default as CmMoonIcon } from '@/lib/components/moon.vue';
+
+export * from '@/lib/components/bars.vue';
+export { default as CmBarsIcon } from '@/lib/components/bars.vue';
+
+export * from '@/lib/components/circleHalf.vue';
+export { default as CmCircleHalf } from '@/lib/components/circleHalf.vue';
+
+export * from '@/lib/components/chevronUp.vue';
+export { default as CmChevronUpIcon } from '@/lib/components/chevronUp.vue';
+
+export * from '@/lib/components/chevronDown.vue';
+export { default as CmChevronDownIcon } from '@/lib/components/chevronDown.vue';
+
+export * from '@/lib/components/chevronLeft.vue';
+export { default as CmChevronLeftIcon } from '@/lib/components/chevronLeft.vue';
+
+export * from '@/lib/components/chevronRight.vue';
+export { default as CmChevronRightIcon } from '@/lib/components/chevronRight.vue';
